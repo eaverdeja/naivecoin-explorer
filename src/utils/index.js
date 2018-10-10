2c42ee0573e82da10a2067c583dfe4f6fe1000cd
Eduardo Verdeja <eaverdeja@gmail.com>
2018-10-09 22:28:51 -0300

    Creating a *hackish* copy-to-clipboard for the hash resumes (f1c...ed4)

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,14 @@
-export const hashResume = (hash, limit = 12) => hash
-  ? `${hash.slice(0, limit)}....${hash.slice(-limit)}`
-  : null;
+import React from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+export const hashResume = (hash, limit = 12) => {
+  const resume = hash
+    ? `${hash.slice(0, limit)}....${hash.slice(-limit)}`
+    : null
+
+  return (
+    <CopyToClipboard style={{ cursor: 'pointer' }} text={hash}>
+      <span>{resume}</span>
+    </CopyToClipboard>
+  )
+}
